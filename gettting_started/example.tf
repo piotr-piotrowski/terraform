b501aaba893398551006fb1accba52882fff5992
Piotr Piotrowski <p.piotrowski@samsung.com>
2019-07-04 13:24:40 +0200

    add s3 bucket on which example instance depends

--- gettting_started/example.tf
+++ gettting_started/example.tf
@@ -3,9 +3,21 @@ provider "aws" {
   region   = "us-east-1"
 }
 
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts,
+  # so this name must be changed before applying this example
+  # to avoid naming conflicts.
+  bucket = "terraform-gettting-started-guide-20190704"
+  acl    = "private"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
+
+  # Tells Terraform that this EC2 instance must be created only after 
+  # the S3 bucket has been created.
+  depends_on = [aws_s3_bucket.example]
 }
 
 resource "aws_eip" "ip" {
