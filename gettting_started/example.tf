bd862713538b5fc5d50a0d095d65d82623e2c430
Piotr Piotrowski <p.piotrowski@samsung.com>
2019-07-04 14:58:10 +0200

    add input variables

--- gettting_started/example.tf
+++ gettting_started/example.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   profile  = "default"
-  region   = "us-east-1"
+  region   = var.region
 }
 
 resource "aws_s3_bucket" "example" {
---
+++ gettting_started/variables.tf
@@ -0,0 +1,7 @@
+#variable "first" {}
+
+#variable "second" {}
+
+variable "region" {
+  default = "us-east-1"
+}
